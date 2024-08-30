a245b626a67645e36a773c1415e838b719ad708f
Akash Mishra <leo.akashmishra@gmail.com>
2024-08-30 19:16:44 +0530

    calculator updat 1

--- Calculator/src/App.js
+++ Calculator/src/App.js
@@ -1,105 +1,27 @@
 import { useRef, useState } from 'react'
-
+import {evaluate} from 'mathjs'
 
 function App() {
-  const [inp1,setInp1] = useState()
-  const [inp2,setInp2] = useState(0)
-  let [operator,setOperator] = useState()
-  const inperef1 = useRef()
+
+  const [inp2,setInp2] = useState(" ")
+ 
   const inperef2 = useRef()
 
   function InputHandler(e){
     setInp2(e.target.value)
+    console.log(inp2);
   }
 
 
   function ClearAll(){
-    setInp1(" ")
     setInp2(" ")
-    setOperator(" ")
-  }
-
-  
-  function add(){
-    setInp1(inp2)
-    setInp2(" ")
-    inperef2.current.focus()
-    setOperator("+")
-    console.log(typeof(operator[0]));
-  }
-
-  
-  
-
-  function substract(){
-    setInp1(inp2)
-    setInp2(" ")
-    inperef2.current.focus()
-    setOperator("-")
-  }
-
-
-  function multiply(){
-    setInp1(inp2)
-    setInp2(" ")
-    inperef2.current.focus()
-    setOperator("×")
-  }
-
-
-  function divide(){
-    setInp1(inp2)
-    setInp2(" ")
-    inperef2.current.focus()
-    setOperator("/")
-  }
-
-  function modulo(){
-    setInp1(inp2)
-    setInp2(" ")
-    inperef2.current.focus()
-    setOperator("-");
+    
   }
 
 
   function final(){
-   
-    switch(operator){
-      case "+":{
-      setInp1(Number(inp1)+Number(inp2))
-      setInp2(" ") 
-      inperef2.current.focus()
-        break;
-      }
-      case "-":{
-        setInp1(Number(inp1)-Number(inp2))
-      setInp2(" ") 
-      inperef2.current.focus()
-        break;
-      }
-      case "×":{
-        setInp1(Number(inp1)*Number(inp2))
-      setInp2(" ") 
-      inperef2.current.focus()
-        break;
-      }
-      case "/":{
-        setInp1(Number(inp1)/Number(inp2))
-      setInp2(" ") 
-      inperef2.current.focus()
-        break;
-      }
-      case "%":{
-        setInp1(Number(inp1)%Number(inp2))
-      setInp2(" ") 
-      inperef2.current.focus()
-        break;
-      }
-      default:{
-        alert("Invalid Operation")
-        break;
-      }
-    }
+    setInp2(evaluate(inp2))
+ 
   }
    
 
@@ -110,34 +32,34 @@ function App() {
 
     <div className='bg-black h-[30%] flex flex-col justify-end mb-2 '>
    
-    <input type="number" ref={inperef1} value={inp1}  autoFocus={false} className='text-end bg-black outline-none caret-transparent select-none selection:bg-transparent  text-white text-[35px] -pr-3 ml-3' readOnly={true} />
     
-   <div className='flex flex-row'>
-   <input type="text" ref={inperef2} value={operator} autoFocus={true} className='p-[0px] text-end bg-black outline-none ml-3 text-white selection:bg-transparent text-[20px]  w-[5%]' readOnly={true}/>
-   <input type="text" ref={inperef2} value={inp2}  onChange={InputHandler}  autoFocus={true} className=' text-end bg-black outline-none ml-3 text-white selection:bg-transparent text-[35px] pr-3 w-[95%]'/>
+    
+   <div className='flex flex-row bg-black  h-full'>
+  
+   <input type="text" ref={inperef2} value={inp2}  onChange={InputHandler}  autoFocus={true} className=' text-end bg-black outline-none ml-3 text-white selection:bg-transparent text-[45px] pr-3 w-[100%]'/>
    </div>
     </div>
 
     <div className='bg-[#22252D] rounded-tl-[10px] rounded-tr-[10px] h-[75%] flex flex-col pb-2 pl-1 pr-1 pt-0.5'>
     <div className='flex flex-row justify-evenly  '>
         <div onClick={ClearAll} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700"><span className='scale-[.9]'>AC</span></div>
-        <div onClick={divide} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">/</div>
-        <div onClick={modulo}  className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">%</div>
-        <div onClick={modulo}  className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700"><span className='scale-[1.3]'>÷</span></div>
+        <div  className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">C</div>
+        <div onClick={()=>setInp2(inp2+"%")}  className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">%</div>
+        <div onClick={()=>setInp2(inp2+"/")}  className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700"><span className='scale-[1.3]'>÷</span></div>
       </div>
 
       <div className='flex flex-row justify-evenly  '>
         <div onClick={()=>setInp2(inp2+"7")} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">7</div>
         <div onClick={()=>setInp2(inp2+"8")} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">8</div>
         <div onClick={()=>setInp2(inp2+"9")} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">9</div>
-        <div onClick={multiply}  className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700"><span className='scale-[1.1]'>×</span></div>
+        <div onClick={()=>setInp2(inp2+"*")}   className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700"><span className='scale-[1.1]'>×</span></div>
       </div>
 
       <div className='flex flex-row justify-evenly'>
         <div onClick={()=>setInp2(inp2+"4")} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">4</div>
         <div onClick={()=>setInp2(inp2+"5")} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">5</div>
         <div onClick={()=>setInp2(inp2+"6")} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">6</div>
-        <div onClick={substract}  className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700"><span className='scale-[1.5]'>-</span></div>
+        <div onClick={()=>setInp2(inp2+"-")}  className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700"><span className='scale-[1.5]'>-</span></div>
       </div>
 
 
@@ -145,12 +67,12 @@ function App() {
         <div onClick={()=>setInp2(inp2+"3")} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">3</div>
         <div onClick={()=>setInp2(inp2+"2")} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">2</div>
         <div onClick={()=>setInp2(inp2+"1")} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">1</div>
-        <div  onClick={add} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700"><span className='scale-[1.1]'>+</span></div>
+        <div onClick={()=>setInp2(inp2+"+")} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700"><span className='scale-[1.1]'>+</span></div>
       </div>
 
       <div className='flex flex-row justify-evenly  '>
         <div className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700"><span className='scale-[.9]'>↺</span></div>
-        <div onClick={()=>setInp2(inp2+"00")}  className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">00</div>
+        <div onClick={()=>setInp2(inp2+"0")}  className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">0</div>
         <div onClick={()=>setInp2(inp2+".")} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700">.</div>
         <div onClick={final} className="bg-black w-[23%] h-[62.5px] mt-2.5 ml-2 text-center flex justify-center items-center mr-2 cursor-pointer hover:bg-slate-800 rounded-[10px] shadow-[0px_0px_2px_black] transition-all border-r-[1px] border-b-[1px] border-gray-700"><span className='scale-[1.1]'>=</span></div>
       </div>
